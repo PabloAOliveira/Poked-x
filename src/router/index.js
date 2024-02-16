ef1bbd6d7275850c7d01ae3d0e6c17d64362b98b
Pablo <oliveiraapablo@gmail.com>
2024-02-16 15:41:03 -0300

    Criando um fake back-end, utilizando o json para isso.

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,21 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 
+import Habilidade from '@/views/Habilidades.vue'
+import Sobre from '@/views/Sobre.vue'
+import Status from '@/views/Status.vue'
+
 const routes = [
   {
     path: '/',
     name: 'Home',
-    component: Home
-  },
+    component: Home,
+    children: [
+      { path: 'habilidades', component: Habilidade },
+      { path: 'sobre', component: Sobre },
+      { path: 'status', component: Status }
+    ]
+  }
 ]
 
 const router = createRouter({
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -9,9 +9,9 @@
 
           <div class="card-body bg-pokebola bg-normal">
             <div class="pokemon">
-              <transition 
-                @after-enter="exibirEvolucoesTransicao"
-                @before-leave="ocultarEvolucoesTransicao"
+              <transition
+                @after-enter="exibirEvolucoes = true"
+                @before-leave="exibirEvolucoes = false"
                 enter-active-class="animate__animated animate__bounceIn"
                 leave-active-class="animate__animated animate__bounceOut"
               >
@@ -19,11 +19,11 @@
               </transition>
 
               <div class="evolucoes">
-                <transition name="fade">
-                  <img src="@/assets/imgs/pokemons/003.png" v-if="exibirEvolucoes">
-                </transition>
-                <transition name="fade">
-                  <img src="@/assets/imgs/pokemons/002.png" v-if="exibirEvolucoes">
+                <transition name="fade" v-for="e in pokemon.evolucoes" :key="e">
+                  <img 
+                    :src="require(`@/assets/imgs/pokemons/${e.toString().padStart(3, '0')}.png`)" 
+                    v-if="exibirEvolucoes"
+                  >
                 </transition>
               </div>
             </div>
@@ -33,10 +33,37 @@
          
           <nav class="nav nav-pills nav-fill">
             <!-- menu de navegação -->
+            <router-link 
+              class="nav-item nav-link text-white" 
+              :to="{ path: '/sobre' }" 
+              exact-active-class="active"
+            >
+              Sobre
+              </router-link>
+              <router-link class="nav-item nav-link text-white" 
+              :to="{ path: '/status' }" 
+              exact-active-class="active"
+            >
+              Status
+              </router-link>
+              <router-link class="nav-item nav-link text-white" 
+              :to="{ path: '/habilidades' }" 
+              exact-active-class="active"
+            >
+              Habilidades
+            </router-link>
           </nav>
 
           <div class="detalhes">
             <!-- exibe dados de acordo com o menu de navegação -->
+            <router-view v-slot="{ Component }">
+              <transition
+                enter-active-class="animate__animated animate__zoomInDown"
+              >
+                <component :is="Component" />
+              </transition>
+            </router-view>
+
           </div>
 
           </div>
@@ -100,46 +127,31 @@ export default {
     exibir: false,
     exibirEvolucoes: false,
     pokemon: {},
-    pokemons: [
-      { id: 1, nome: 'Bulbasaur', tipo: 'grama', imagem: '001.png', evolucoes: [2,3] },
-      { id: 2, nome: 'Ivysaur', tipo: 'grama', imagem: '002.png', evolucoes: [3] },
-      { id: 3, nome: 'Venusaur', tipo: 'grama', imagem: '003.png', evolucoes: [] },
-      { id: 4, nome: 'Charmander', tipo: 'fogo', imagem: '004.png', evolucoes: [5, 6] },
-      { id: 5, nome: 'Charmeleon', tipo: 'fogo', imagem: '005.png', evolucoes: [6] },
-      { id: 6, nome: 'Charizard', tipo: 'fogo', imagem: '006.png', evolucoes: [] },
-      { id: 7, nome: 'Squirtle', tipo: 'agua', imagem: '007.png', evolucoes: [8,9] },
-      { id: 8, nome: 'Wartortle', tipo: 'agua', imagem: '008.png', evolucoes: [9] },
-      { id: 9, nome: 'Blastoise', tipo: 'agua', imagem: '009.png', evolucoes: [] },
-      { id: 10, nome: 'Caterpie', tipo: 'inseto', imagem: '010.png', evolucoes: [11,12] },
-      { id: 11, nome: 'Metapod', tipo: 'inseto', imagem: '011.png', evolucoes: [12] },
-      { id: 12, nome: 'Butterfree', tipo: 'inseto', imagem: '012.png', evolucoes: [] },
-      { id: 13, nome: 'Weedle', tipo: 'inseto', imagem: '013.png', evolucoes: [14,15] },
-      { id: 14, nome: 'Kakuna', tipo: 'inseto', imagem: '014.png', evolucoes: [15] },
-      { id: 15, nome: 'Beedrill', tipo: 'inseto', imagem: '015.png', evolucoes: [] },
-      { id: 16, nome: 'Pidgey', tipo: 'normal', imagem: '016.png', evolucoes: [17,18] },
-      { id: 17, nome: 'Pidgeotto', tipo: 'normal', imagem: '017.png', evolucoes: [18] },
-      { id: 18, nome: 'Pidgeot', tipo: 'normal', imagem: '018.png', evolucoes: [] }
-    ]
+    pokemons: []
   }),
+  created() {
+    fetch('http://localhost:3000/pokemons')
+      .then(response => {
+        return response.json()
+      })
+      .then(data => {
+        this.pokemons = data
+      })
+  },
   methods: {
-    exibirEvolucoesTransicao() {
-      this.exibirEvolucoes = true
-    },
-    ocultarEvolucoesTransicao() {
-      this.exibirEvolucoes = false
-    },
     analisarPokemon(p) {
 
-      //se o pokemon atual é diferente do pokemon clicado
+      //se o pokémon atua é diferente do pokémon clicado
       //se o atributo exibir é true
-      if(this.pokemon.id != p.id) {
+      if((this.pokemon.id != p.id) && this.exibir) {
 
-        setTimeout(() =>{
+        setTimeout(() => {
           this.analisarPokemon(p)
         }, 1000)
       }
       this.pokemon = p
-      this.exibir = !this.exibir //função toggle
+      this.exibir = !this.exibir
+      this.exibirEvolucoes = !this.exibirEvolucoes
     }
   }
 }
@@ -264,7 +276,6 @@ body {
   cursor: pointer;
   max-width: 100%;
   max-height: 100%;
-  float: right;
 }
 
 </style>
